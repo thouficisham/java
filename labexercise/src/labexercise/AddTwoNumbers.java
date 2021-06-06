955bf85c0830e9ec58dfa866c6cb59ad82cb9c0d
my pc <my pc@DESKTOP-FTU63R4>
2021-06-06 14:31:03 +0530

    [lab exercise] lab exercise

---
+++ labexercise/src/labexercise/AddTwoNumbers.java
@@ -0,0 +1,14 @@
+package labexercise;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number1 =50, number2 = 150, sum;
+		sum = number1 + number2;
+		System.out.println("sum of these numbers: "+sum);
+
+
+	}
+
+}
